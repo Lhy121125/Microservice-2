eb720b5bf8575795455a234db21666d4bc07cb51
Lixuan Yang <36718986+LixuanYang@users.noreply.github.com>
2023-10-19 16:29:13 -0400

    upload dummy feature deployment for sprint1

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def root():  # put application's code here
+    return 'This is the google app engine feature. To be continued...'
+
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=8080,debug=True)
